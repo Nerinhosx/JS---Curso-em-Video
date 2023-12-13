b21d5ec54a1d52a2712597fca149a6bb48622af1
Nerinhosx <nerooliveira.arthur@gmail.com>
2023-12-13 14:14:29 -0300

    1ª versão do exemplo 8 concluída (Condição simples).

---
+++ Aula_11/Ex_008.js
@@ -0,0 +1,7 @@
+var vel = 78.2;
+console.log(`\nA velocidade do seu carro é ${vel}Km/h\n`);
+if(vel > 60)//Condição simples
+{
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!\n');
+}
+console.log('Dirija sempre usando cinto de segurança.\n');
