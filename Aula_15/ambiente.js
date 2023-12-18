38d4dcf15eee11487447174dc17a3f6216a0acca
Nerinhosx <nerooliveira.arthur@gmail.com>
2023-12-18 14:10:42 -0300

    Criação de ambiente para teste de variáveis compostas (arrays).

---
+++ Aula_15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3];
+num.sort();
+num.push(1);
+console.log(`O vetor tem ${num.length} posições.`);
+console.log(`O 1º valor do vetor é ${num[0]}`);
+
+/*
+num.push(7) ---> Adiciona o valor entre parênteses na última posição do array.
+
+num.length ---> Retorna o tamanho do array (quantas posições têm).
+
+num.sort ---> Ordena o array na forma "crescente".
+*/
