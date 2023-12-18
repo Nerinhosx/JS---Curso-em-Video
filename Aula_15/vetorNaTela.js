6008472b7ab3a9f76af1626eb87b74c1677d6430
Nerinhosx <nerooliveira.arthur@gmail.com>
2023-12-18 14:20:29 -0300

    Criação de ambiente de experimentação das propriedades dos arrays.

---
+++ Aula_15/vetorNaTela.js
@@ -0,0 +1,18 @@
+let val = [8, 1, 7, 4, 2, 9];
+
+/*
+console.log(val[0]);
+console.log(val[1]);
+console.log(val[2]);
+console.log(val[3]);
+console.log(val[4]);
+console.log(val[5]);
+
+for(let pos = 0; pos < val.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${val[pos]}`);
+}
+*/
+
+for(let pos in val){
+    console.log(`A posição ${pos} tem o valor ${val[pos]}`);
+}
